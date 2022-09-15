55807b22ec5144ce3c88f6e11c4f259320568e22
Emmanuel Goldstein <emgoldstein42@proton.me>
2022-09-15 00:52:16 +0000

    trying again with custom scaling

--- startkde/startplasma-wayland.cpp
+++ startkde/startplasma-wayland.cpp
@@ -67,9 +67,25 @@ int main(int argc, char **argv)
     setupPlasmaEnvironment();
     runStartupConfig();
 
-    qputenv("PLASMA_USE_QT_SCALING", "2");
-    qputenv("GDK_SCALE", "2");
-    qputenv("GDK_DPI_SCALE", "2");
+    qputenv("PLASMA_USE_QT_SCALING", "1");
+    qputenv("GDK_SCALE", "1");
+    qputenv("GDK_DPI_SCALE", "1");
+
+    if (qEnvironmentVariableIsSet("ROSA_PLASMA_USE_QT_SCALING")) {
+        qputenv("PLASMA_USE_QT_SCALING", qgetenv("ROSA_PLASMA_USE_QT_SCALING"));
+    }
+
+    if (qEnvironmentVariableIsSet("ROSA_GDK_SCALE")) {
+        qputenv("GDK_SCALE", qgetenv("ROSA_GDK_SCALE"));
+    }
+
+    if (qEnvironmentVariableIsSet("ROSA_GDK_DPI_SCALE")) {
+        qputenv("GDK_DPI_SCALE", qgetenv("ROSA_GDK_DPI_SCALE"));
+    }
+
+    if (qEnvironmentVariableIsSet("ROSA_QT_WAYLAND_FORCE_DPI")) {
+        qputenv("QT_WAYLAND_FORCE_DPI", qgetenv("ROSA_QT_WAYLAND_FORCE_DPI"));
+    }
 
     qputenv("XDG_SESSION_TYPE", "wayland");
 
